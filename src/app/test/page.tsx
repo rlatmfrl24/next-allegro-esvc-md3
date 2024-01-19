dcab366abfdaf927664f5702779180f482015b04
SulfurBottom <397love@gmail.com>
2024-01-19 13:48:49 +0900

    md3 스타일 정립 및 중간 저장

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,59 +4,49 @@ import {
   MdElevationButton,
   MdFilledButton,
   MdOutlinedButton,
+  MdRippleEffect,
+  MdTextButton,
+  createMDTheme,
 } from "../util/md3";
-import {
-  argbFromHex,
-  themeFromSourceColor,
-  applyTheme,
-} from "@material/material-color-utilities";
 
 export default function Test() {
-  function createTheme() {
-    const theme = themeFromSourceColor(argbFromHex("#120891"), []);
-    console.log(JSON.stringify(theme, null, 2));
-
-    // Check if the user has dark mode turned on
-    const systemDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-
-    // Apply the theme to the body by updating custom properties for material tokens
-    applyTheme(theme, { target: document.body, dark: systemDark });
-  }
-
   return (
-    <div className="flex gap-1 p-2">
-      <p className="flex flex-col font-suit text-4xl">
-        <span className="font-thin">Booking</span>
-        <span className="font-light">Booking</span>
-        <span>Booking</span>
-        <span className="font-semibold">Booking</span>
-        <span className="font-bold">Booking</span>
-        <span className="font-extrabold">Booking</span>
-        <span className="font-black">Booking</span>
-      </p>
-      <p className="flex flex-col font-pretendard text-4xl">
-        <span className="font-thin">Booking</span>
-        <span className="font-light">Booking</span>
-        <span>Booking</span>
-        <span className="font-semibold">Booking</span>
-        <span className="font-bold">Booking</span>
-        <span className="font-extrabold">Booking</span>
-        <span className="font-black">Booking</span>
-      </p>
+    <div className="flex flex-col gap-2 p-2">
+      <div className="flex">
+        <p className="flex flex-col font-suit text-4xl">
+          <span className="font-thin">Booking</span>
+          <span className="font-light">Booking</span>
+          <span>Booking</span>
+          <span className="font-semibold">Booking</span>
+          <span className="font-bold">Booking</span>
+          <span className="font-extrabold">Booking</span>
+          <span className="font-black">Booking</span>
+        </p>
+        <p className="flex flex-col font-pretendard text-4xl">
+          <span className="font-thin">Booking</span>
+          <span className="font-light">Booking</span>
+          <span>Booking</span>
+          <span className="font-semibold">Booking</span>
+          <span className="font-bold">Booking</span>
+          <span className="font-extrabold">Booking</span>
+          <span className="font-black">Booking</span>
+        </p>
+      </div>
 
-      <div className="flex gap-1 h-fit">
-        <MdFilledButton
-          className="font-suit"
-          onClick={() => {
-            createTheme();
-          }}
-        >
-          Click me
-        </MdFilledButton>
-        <MdElevationButton className="font-suit">Click me</MdElevationButton>
-        <MdOutlinedButton className="font-suit">Click me</MdOutlinedButton>
+      <div className="flex flex-col gap-2 ">
+        <div className="flex gap-2">
+          <MdFilledButton
+            className="font-suit text-secondary"
+            onClick={() => {
+              createMDTheme("#f44336", "#298189");
+            }}
+          >
+            Click me
+          </MdFilledButton>
+          <MdElevationButton className="font-suit">Click me</MdElevationButton>
+          <MdOutlinedButton className="font-suit">Click me</MdOutlinedButton>
+          <MdTextButton className="font-suit">Click me</MdTextButton>
+        </div>
       </div>
     </div>
   );
--- src/app/util/md3.tsx
+++ src/app/util/md3.tsx
@@ -2,9 +2,23 @@
 
 import React from "react";
 import { createComponent } from "@lit/react";
+import { MdRipple } from "@material/web/ripple/ripple.js";
 import { MdFilledButton as MdFilledButtonWebComponent } from "@material/web/button/filled-button.js";
 import { MdElevatedButton as MdElevatedButtonWebComponent } from "@material/web/button/elevated-button.js";
 import { MdOutlinedButton as MdOutlinedButtonWebComponent } from "@material/web/button/outlined-button.js";
+import { MdTextButton as MdTextButtonWebComponent } from "@material/web/button/text-button.js";
+
+import {
+  applyTheme,
+  argbFromHex,
+  themeFromSourceColor,
+} from "@material/material-color-utilities";
+
+export const MdRippleEffect = createComponent({
+  tagName: "md-ripple",
+  elementClass: MdRipple,
+  react: React,
+});
 
 export const MdFilledButton = createComponent({
   tagName: "md-filled-button",
@@ -23,3 +37,34 @@ export const MdOutlinedButton = createComponent({
   elementClass: MdOutlinedButtonWebComponent,
   react: React,
 });
+
+export const MdTextButton = createComponent({
+  tagName: "md-text-button",
+  elementClass: MdTextButtonWebComponent,
+  react: React,
+});
+
+export function createMDTheme(
+  sourceColor: string,
+  pointColor: string,
+  isContainDarkMode?: boolean
+) {
+  const theme = themeFromSourceColor(argbFromHex(sourceColor), [
+    {
+      name: "point",
+      value: argbFromHex(pointColor),
+      blend: true,
+    },
+  ]);
+  console.log(JSON.stringify(theme, null, 2));
+
+  // Check if the user has dark mode turned on
+  const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  // Apply the theme to the body by updating custom properties for material tokens
+  if (isContainDarkMode !== undefined && isContainDarkMode === true) {
+    applyTheme(theme, { target: document.body, dark: systemDark });
+  } else {
+    applyTheme(theme, { target: document.body });
+  }
+}
