90835192bd630771f7fea573082dadef671f0724
SulfurBottom <397love@gmail.com>
2024-02-07 08:19:13 +0900

    Refactor code to improve performance and readability

--- src/app/main/schedule/vessel/page.tsx
+++ src/app/main/schedule/vessel/page.tsx
@@ -3,7 +3,7 @@
 import { MdSingleDatePicker } from "@/app/components/datepickers/date-picker";
 import { MdRangeDatePicker } from "@/app/components/datepickers/range-picker";
 import { MdTypography } from "@/app/components/typography";
-import { MdFilledButton, MdOutlinedCard } from "@/app/util/md3";
+import { MdOutlinedCard } from "@/app/util/md3";
 import {
   AnimatePresence,
   motion,
@@ -31,100 +31,107 @@ export default function VesselSchedule() {
   });
 
   return (
-    <div
-      ref={scrollContainerRef}
-      className="relative overflow-auto items-center flex flex-col gap-3 flex-1 w-full"
-    >
-      <div className="relative w-full max-w-[1400px] p-6">
-        <div className="sticky top-0 pt-3 z-50 h-96">
-          <motion.div layout>
-            {isScrollTop ? (
-              <MdOutlinedCard className="p-5 my-5 z-10">
-                <MdSingleDatePicker
-                  className="z-20"
-                  handleDateChange={setFirstDate}
-                />
-                <MdSingleDatePicker className="z-10" />
-                <MdRangeDatePicker />
-              </MdOutlinedCard>
-            ) : (
-              <div className="border">
-                <MdTypography variant="title" size="large">
-                  {firstDate ? firstDate.toISODate() : "No date selected"}
-                </MdTypography>
-              </div>
-            )}
+    <div className="relative flex-1 flex flex-col w-full">
+      <AnimatePresence>
+        {!isScrollTop && (
+          <motion.div
+            initial={{ y: -190 }}
+            animate={{ y: 0 }}
+            exit={{ y: -200 }}
+            transition={{
+              duration: 0.5,
+              type: "spring",
+            }}
+            className="absolute top-0 w-full h-48 bg-red-300 z-20"
+          >
+            1
           </motion.div>
+        )}
+      </AnimatePresence>
+      <div
+        ref={scrollContainerRef}
+        className="relative flex justify-center flex-auto h-0 overflow-auto w-full p-6"
+      >
+        <div className="max-w-[1400px] w-full">
+          <MdOutlinedCard className={`flex-1 flex flex-col gap-3 z-10 p-6`}>
+            <>
+              <MdSingleDatePicker
+                className="z-20"
+                handleDateChange={setFirstDate}
+              />
+              <MdSingleDatePicker className="z-10" />
+              <MdRangeDatePicker />
+            </>
+          </MdOutlinedCard>
+          <MdOutlinedCard className="flex flex-col gap-3 p-5 my-5 ">
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+            <MdTypography variant="title" size="large">
+              {firstDate ? firstDate.toISODate() : "No date selected"}
+            </MdTypography>
+          </MdOutlinedCard>
         </div>
-
-        <MdOutlinedCard className="flex flex-col gap-3 p-5 my-5 ">
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-          <MdTypography variant="title" size="large">
-            {firstDate ? firstDate.toISODate() : "No date selected"}
-          </MdTypography>
-        </MdOutlinedCard>
       </div>
     </div>
   );
