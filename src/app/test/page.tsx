1bc7976dcf6660a83c1ea2563aea453885d202c2
SulfurBottom <397love@gmail.com>
2024-03-15 14:33:40 +0900

    Refactor imports and remove unused code

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,34 +1,10 @@
 "use client";
 
-import {
-  MdElevatedCard,
-  MdElevationButton,
-  MdFilledButton,
-  MdFilledCard,
-  MdIcon,
-  MdNavigationBar,
-  MdNavigationTab,
-  MdOutlinedButton,
-  MdOutlinedCard,
-  MdOutlinedSegmentedButton,
-  MdOutlinedSegmentedButtonSet,
-  MdOutlinedTextField,
-  MdTextButton,
-} from "../util/md3";
+import { MdFilledButton } from "../util/md3";
 import { createMDTheme, applyPresetTheme } from "../util/theme";
 import { useEffect, useState } from "react";
 import { HexColorPicker } from "react-colorful";
 import { useRouter } from "next/navigation";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import SettingsIcon from "@mui/icons-material/Settings";
-import {
-  MdSegmentedButton,
-  MdSegmentedButtons,
-} from "../components/unused/segmented-button";
 
 export default function Test() {
   const [color, setColor] = useState("#009FE8");
@@ -59,7 +35,7 @@ export default function Test() {
   }
 
   return (
-    <div className="flex flex-1 flex-col gap-2 p-2">
+    <div className="flex flex-1 flex-col gap-2 p-2 overflow-auto">
       <MdFilledButton
         onClick={() => {
           router.push("/");
@@ -72,50 +48,6 @@ export default function Test() {
       </div>
 
       <HexTest />
-      <div className="flex gap-4">
-        <p className="flex flex-col font-pretendard text-4xl">
-          <span className="font-thin">Booking</span>
-          <span className="font-light">Booking</span>
-          <span>Booking</span>
-          <span className="font-semibold">Booking</span>
-          <span className="font-bold">Booking</span>
-          <span className="font-extrabold">Booking</span>
-          <span className="font-black">Booking</span>
-        </p>
-        <p className="flex flex-col font-pretendard text-4xl">
-          <span className="font-thin">Booking</span>
-          <span className="font-light">Booking</span>
-          <span>Booking</span>
-          <span className="font-semibold">Booking</span>
-          <span className="font-bold">Booking</span>
-          <span className="font-extrabold">Booking</span>
-          <span className="font-black">Booking</span>
-        </p>
-      </div>
-
-      <div className="flex gap-2">
-        <MdFilledButton>Click me</MdFilledButton>
-        <MdElevationButton>Click me</MdElevationButton>
-        <MdOutlinedButton>Click me</MdOutlinedButton>
-        <MdTextButton>Click me</MdTextButton>
-      </div>
-
-      <div className="flex gap-2 font-pretendard font-bold">
-        <MdOutlinedTextField label="Label" className="h-fit" />
-        <MdOutlinedTextField label="Label" className="h-fit" disabled />
-        <MdOutlinedTextField label="Label" className="h-fit" error />
-        <MdOutlinedTextField
-          label="Label"
-          className="h-fit"
-          supportingText="Required"
-        />
-        <MdOutlinedTextField
-          label="Label"
-          className="h-fit"
-          prefixText="US"
-          suffixText="USD"
-        />
-      </div>
 
       <div className="flex gap-2">
         <button
@@ -142,66 +74,6 @@ export default function Test() {
         >
           Preset 3
         </button>
-      </div>
-      <div className="flex gap-2 mt-4">
-        <MdElevatedCard className=" w-48 h-24 flex justify-center items-center">
-          Elevated Card
-        </MdElevatedCard>
-        <MdFilledCard className="w-48 h-24 flex justify-center items-center">
-          Filled Card
-        </MdFilledCard>
-        <MdOutlinedCard className="w-48 h-24 flex justify-center items-center">
-          Outlined Card
-        </MdOutlinedCard>
-      </div>
-      <div className="flex gap-2 mt-4">
-        <MdNavigationBar activeIndex={1} hideInactiveLabels>
-          <MdNavigationTab label="Calendar" showBadge badgeValue="4">
-            <MdIcon slot="active-icon">
-              <CalendarTodayIcon />
-            </MdIcon>
-            <MdIcon slot="inactive-icon">
-              <CalendarTodayOutlinedIcon />
-            </MdIcon>
-          </MdNavigationTab>
-          <MdNavigationTab label="User" showBadge>
-            <MdIcon slot="active-icon">
-              <AccountCircleIcon />
-            </MdIcon>
-            <MdIcon slot="inactive-icon">
-              <AccountCircleOutlinedIcon />
-            </MdIcon>
-          </MdNavigationTab>
-          <MdNavigationTab label="Setting">
-            <MdIcon slot="active-icon">
-              <SettingsIcon />
-            </MdIcon>
-            <MdIcon slot="inactive-icon">
-              <SettingsOutlinedIcon />
-            </MdIcon>
-          </MdNavigationTab>
-        </MdNavigationBar>
-      </div>
-      <div className="flex gap-2 mt-4">
-        <MdOutlinedSegmentedButtonSet className="flex-1">
-          <MdOutlinedSegmentedButton label="111"></MdOutlinedSegmentedButton>
-          <MdOutlinedSegmentedButton label="222"></MdOutlinedSegmentedButton>
-          <MdOutlinedSegmentedButton label="333"></MdOutlinedSegmentedButton>
-          <MdOutlinedSegmentedButton
-            label="444"
-            disabled
-          ></MdOutlinedSegmentedButton>
-        </MdOutlinedSegmentedButtonSet>
-      </div>
-      <div className="flex gap-2 mt-4 ">
-        <MdSegmentedButtons value="111" className="flex-1">
-          <MdSegmentedButton id="111">111</MdSegmentedButton>
-          <MdSegmentedButton id="222">222</MdSegmentedButton>
-          <MdSegmentedButton id="333">333</MdSegmentedButton>
-          <MdSegmentedButton id="444" disabled>
-            444
-          </MdSegmentedButton>
-        </MdSegmentedButtons>
       </div>
     </div>
   );
