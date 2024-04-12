484cb44acee4c12ab0f1b8020ffcbed9fd0e12fb
SulfurBottom <397love@gmail.com>
2024-04-12 10:41:02 +0900

    Refactor table components and add hover and select functionality in TableBody.tsx

--- src/app/components/table/table-body.tsx
+++ src/app/components/table/table-body.tsx
@@ -1,13 +1,76 @@
-import { Table, flexRender } from "@tanstack/react-table";
+import { Cell, Row, Table, flexRender } from "@tanstack/react-table";
 import { getCommonPinningStyles } from "./util";
-import { memo } from "react";
+import { CSSProperties, memo, use, useEffect, useState } from "react";
 
 export const TableBody = ({ table }: { table: Table<any> }) => {
+  const [hoverInfo, setHoverInfo] = useState<{
+    row: Row<any>;
+    cell: Cell<any, unknown>;
+  } | null>(null);
+
+  const [selectInfo, setSelectInfo] = useState<{
+    row: Row<any>;
+    cell: Cell<any, unknown>;
+  } | null>(null);
+
+  useEffect(() => {
+    setHoverInfo(null);
+    setSelectInfo(null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [table.getState().pagination]);
+
+  useEffect(() => {
+    console.log(hoverInfo);
+  }, [hoverInfo]);
+
+  useEffect(() => {
+    console.log(selectInfo);
+  }, [selectInfo]);
+
+  function getBgColor(
+    isSelected: boolean,
+    isHovered: boolean,
+    type: "cell" | "row"
+  ) {
+    if (type === "cell") {
+      // Cell Style
+      if (isSelected) {
+        return `color-mix(in srgb, var(--md-sys-color-primary) 12%, transparent)`;
+      } else {
+        if (isHovered) {
+          return `color-mix(in srgb, var(--md-sys-color-on-surface) 12%, transparent)`;
+        } else {
+          return ``;
+        }
+      }
+    } else {
+      // Row Style
+      if (isSelected) {
+        return `color-mix(in srgb, var(--md-sys-color-primary) 8%, transparent)`;
+      } else {
+        if (isHovered) {
+          return `color-mix(in srgb, var(--md-sys-color-on-surface) 8%, transparent)`;
+        } else {
+          return `var(--md-sys-color-surface)`;
+        }
+      }
+    }
+  }
+
   return (
     <tbody>
       {table.getRowModel().rows.map((row) => {
         return (
-          <tr key={row.id} className="group">
+          <tr
+            key={row.id}
+            style={{
+              backgroundColor: getBgColor(
+                row === selectInfo?.row,
+                row === hoverInfo?.row,
+                "row"
+              ),
+            }}
+          >
             {row.getVisibleCells().map((cell) => {
               return (
                 <td
@@ -16,9 +79,21 @@ export const TableBody = ({ table }: { table: Table<any> }) => {
                     // width: cell.column.getSize(),
                     width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
                     ...getCommonPinningStyles(cell.column),
+                    backgroundColor: getBgColor(
+                      cell === selectInfo?.cell,
+                      cell === hoverInfo?.cell,
+                      "cell"
+                    ),
                   }}
-                  className="group-hover:bg-surfaceContainer p-2"
+                  className="p-2"
+                  onMouseEnter={(e) => {
+                    setHoverInfo({ row, cell });
+                  }}
+                  onMouseLeave={(e) => {
+                    setHoverInfo((prev) => (prev?.row === row ? null : prev));
+                  }}
                   onClick={(e) => {
+                    setSelectInfo({ row, cell });
                     row.toggleSelected();
                   }}
                 >
