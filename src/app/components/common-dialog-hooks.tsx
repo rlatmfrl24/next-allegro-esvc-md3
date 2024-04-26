7e56df7011c60ed476218e6d452114fedbf4b690
SulfurBottom <397love@gmail.com>
2024-04-26 14:54:25 +0900

    Implement Place Info Dialog and Vessel Info Dialog logic in common-dialog-hooks.tsx

--- src/app/components/common-dialog-hooks.tsx
+++ src/app/components/common-dialog-hooks.tsx
@@ -1,15 +1,62 @@
+import { useState } from "react";
+
 import Portal from "@/app/components/portal";
 import VesselScheduleDialog from "@/app/main/schedule/popup/vessel-schedule";
 import { createDummyVesselSchedules } from "@/app/main/schedule/util";
-import { VesselInfoType } from "@/app/util/typeDef/schedule";
-import { useState } from "react";
+import {
+  PlaceInformationType,
+  VesselInfoType,
+} from "@/app/util/typeDef/schedule";
+
+import PlaceInformationDialog from "../main/schedule/popup/place-information";
+import VesselInformationDialog from "../main/schedule/popup/vessel-information";
 
 export const usePlaceInfoDialog = () => {
-  //TODO: Implement Place Info Dialog logic here
+  const [isPlaceInfoDialogOpen, setIsPlaceInfoDialogOpen] = useState(false);
+  const [currentPlace, setCurrentPlace] = useState({} as PlaceInformationType);
+
+  function renderDialog() {
+    return (
+      <Portal selector="#main-container">
+        <PlaceInformationDialog
+          open={isPlaceInfoDialogOpen}
+          handleOpen={setIsPlaceInfoDialogOpen}
+          data={currentPlace}
+        />
+      </Portal>
+    );
+  }
+
+  return {
+    renderDialog,
+    setCurrentPlace,
+    setIsPlaceInfoDialogOpen,
+  };
 };
 
 export const useVesselInfoDialog = () => {
-  //TODO: Implement Vessel Info Dialog logic here
+  const [isVesselInfoDialogOpen, setIsVesselInfoDialogOpen] = useState(false);
+  const [currentVessel, setCurrentVessel] = useState<VesselInfoType>(
+    {} as VesselInfoType
+  );
+
+  function renderDialog() {
+    return (
+      <Portal selector="#main-container">
+        <VesselInformationDialog
+          open={isVesselInfoDialogOpen}
+          handleOpen={setIsVesselInfoDialogOpen}
+          data={currentVessel}
+        />
+      </Portal>
+    );
+  }
+
+  return {
+    renderDialog,
+    setCurrentVessel,
+    setIsVesselInfoDialogOpen,
+  };
 };
 
 export const useVesselScheduleDialog = () => {
