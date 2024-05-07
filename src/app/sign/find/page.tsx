3863fb19dc184429a2e0d0e96577d6b587d09e9f
SulfurBottom <397love@gmail.com>
2024-05-07 15:21:13 +0900

    Refactor FindIDPassword component and add CheckApprover functionality

--- src/app/sign/find/page.tsx
+++ src/app/sign/find/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import NAOutlinedListBox from "@/app/components/na-outline-listbox";
 import { NAOutlinedTextField } from "@/app/components/na-textfield";
+import { useSimpleTable } from "@/app/components/table/simple-table";
 import { MdTypography } from "@/app/components/typography";
+import { DividerComponent } from "@/app/main/booking/information/components/base";
 import {
   MdDialog,
   MdElevatedCard,
@@ -13,8 +16,9 @@ import {
 } from "@/app/util/md3";
 import { faker } from "@faker-js/faker";
 import { PasswordOutlined, PersonSearchOutlined } from "@mui/icons-material";
+import { createColumnHelper } from "@tanstack/react-table";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function FindIDPassword() {
   const [findTarget, setFindTarget] = useState<"id" | "password">("id");
@@ -52,7 +56,7 @@ export default function FindIDPassword() {
             }}
           />
           <div className="flex gap-2 w-full ">
-            <MdTextButton>Check Appover</MdTextButton>
+            <CheckApprover />
             <div className="flex-1"></div>
             <MdOutlinedButton
               onClick={() => {
@@ -122,7 +126,7 @@ export default function FindIDPassword() {
               </>
             ) : (
               <>
-                <MdTextButton>Check Approver</MdTextButton>
+                <CheckApprover />
                 <MdFilledButton
                   onClick={() => {
                     setIsDialogOpen(false);
@@ -175,7 +179,7 @@ export default function FindIDPassword() {
             }}
           />
           <div className="flex gap-2 w-full">
-            <MdTextButton>Check Appover</MdTextButton>
+            <CheckApprover />
             <div className="flex-1"></div>
             <MdOutlinedButton
               onClick={() => {
@@ -228,7 +232,7 @@ export default function FindIDPassword() {
               </MdFilledButton>
             ) : (
               <>
-                <MdTextButton>Check Approver</MdTextButton>
+                <CheckApprover />
                 <MdFilledButton
                   onClick={() => {
                     setIsDialogOpen(false);
@@ -272,16 +276,98 @@ export default function FindIDPassword() {
 const CheckApprover = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
+  type CheckApproverProps = {
+    office: string;
+    country: string;
+    name: string;
+    email: string;
+    tel: string;
+  };
+
+  const tempData: CheckApproverProps[] = useMemo(() => {
+    return Array.from({ length: 10 }, () => ({
+      office: faker.location.city() + " Office",
+      country: faker.location.country(),
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      tel: faker.phone.number(),
+    }));
+  }, []);
+
+  const columnHelper = createColumnHelper<CheckApproverProps>();
+
+  const columnDefs = [
+    columnHelper.accessor("office", {
+      header: "Office",
+    }),
+    columnHelper.accessor("country", {
+      header: "Country",
+    }),
+    columnHelper.accessor("name", {
+      header: "Name",
+    }),
+    columnHelper.accessor("email", {
+      header: "Email",
+    }),
+    columnHelper.accessor("tel", {
+      header: "Tel",
+    }),
+  ];
+
+  const { renderTable } = useSimpleTable({
+    data: tempData,
+    columns: columnDefs,
+  });
+
   return (
     <>
-      <MdTextButton>Check Approver</MdTextButton>
+      <MdTextButton
+        onClick={() => {
+          setIsDialogOpen(true);
+        }}
+      >
+        Check Approver
+      </MdTextButton>
       <MdDialog
         open={isDialogOpen}
         closed={() => {
           setIsDialogOpen(false);
         }}
+        className="min-w-fit"
       >
-        <div slot="headline"></div>
+        <div slot="headline">Check Approver</div>
+        <div slot="content" className="flex flex-col gap-4">
+          <input className="h-0" />
+          <div className="flex gap-2">
+            <NAOutlinedListBox
+              initialValue="All"
+              options={tempData
+                .map((item) => {
+                  return item.country;
+                })
+                .filter((value, index, self) => self.indexOf(value) === index)}
+              label="Country"
+              className="flex-1"
+            />
+            <NAOutlinedListBox
+              initialValue="All"
+              options={tempData
+                .map((item) => {
+                  return item.office;
+                })
+                .filter((value, index, self) => self.indexOf(value) === index)}
+              label="Contact Office"
+              className="flex-1"
+            />
+          </div>
+          <div className="flex justify-end gap-2">
+            <MdTextButton>Reset</MdTextButton>
+            <MdFilledButton>Search</MdFilledButton>
+          </div>
+          <DividerComponent />
+          {renderTable()}
+        </div>
+        <div slot="actions"></div>
       </MdDialog>
     </>
   );
