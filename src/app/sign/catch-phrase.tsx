a98a03ea3db526b3ecc367ea8413c5dd74ccc103
SulfurBottom <397love@gmail.com>
2024-05-07 16:04:53 +0900

    Refactor sign up and sign in pages, add CheckApprover functionality, and improve button styling

--- src/app/sign/catch-phrase.tsx
+++ src/app/sign/catch-phrase.tsx
@@ -1,6 +1,6 @@
 const CatchPhrase = () => {
   return (
-    <div className="absolute left-0 top-1/3 z-10 bg-black bg-opacity-50 px-20 py-9 text-white font-pretendard rounded-r-2xl">
+    <div className="absolute left-0 top-1/3 bg-black bg-opacity-50 px-20 py-9 text-white font-pretendard rounded-r-2xl">
       <h1 className="text-5xl font-semibold mb-8">
         Transfar Shipping,
         <br /> an ocean carrier you can trust!
---
+++ src/app/sign/up/main.tsx
@@ -0,0 +1,54 @@
+import Portal from "@/app/components/portal";
+import { MdTypography } from "@/app/components/typography";
+import { MdCheckbox, MdDialog } from "@/app/util/md3";
+import { useCallback, useState } from "react";
+
+export const useRegister = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [currentStep, setCurrentStep] = useState<
+    "policy" | "form" | "preview" | "complete"
+  >("policy");
+
+  const openDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  function renderRegisterDialog() {
+    return (
+      <Portal selector="#nav-container">
+        <MdDialog
+          open={isDialogOpen}
+          closed={() => {
+            setIsDialogOpen(false);
+          }}
+          className="relative z-20"
+        >
+          <div slot="headline">Sign Up</div>
+          <div slot="content"></div>
+          <div slot="actions">
+            {
+              {
+                policy: (
+                  <>
+                    <MdTypography tag="label" variant="label" size="large">
+                      <MdCheckbox />I have read, understood and agree to the
+                      ‘Privacy and Security Policy’
+                    </MdTypography>
+                  </>
+                ),
+                form: <></>,
+                preview: <></>,
+                complete: <></>,
+              }[currentStep]
+            }
+          </div>
+        </MdDialog>
+      </Portal>
+    );
+  }
+
+  return {
+    openDialog,
+    renderRegisterDialog,
+  };
+};
