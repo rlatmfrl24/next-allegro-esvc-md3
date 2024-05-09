f1cb4f9e059b7927f7b6b0d58970331b946b3859
SulfurBottom <397love@gmail.com>
2024-05-09 09:04:41 +0900

    Refactor guide download page, update page title and container styling

--- src/app/main/quick/guide/page.tsx
+++ src/app/main/quick/guide/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import PageTitle from "@/app/components/title-components";
+import styles from "@/app/styles/base.module.css";
+import classNames from "classnames";
+
 export default function GuideDownloadPage() {
+  const cx = classNames.bind(styles);
+
   return (
-    <div>
-      <h1>Download Guide</h1>
-      <p>Download guide content</p>
+    <div aria-label="container" className={cx(styles.container, "h-full")}>
+      <PageTitle title="e-Service Guide" />
+      <div className={cx(styles.area, "flex-1")}></div>
     </div>
   );
 }
