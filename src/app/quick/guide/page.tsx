50e12665f0ccb8da021b28a90c54ff27cc6a0747
SulfurBottom <397love@gmail.com>
2024-05-24 13:13:45 +0900

    chore: Update GuideDownloadPage to use OverlayScrollbarsComponent for better scrolling experience

--- src/app/quick/guide/page.tsx
+++ src/app/quick/guide/page.tsx
@@ -8,6 +8,8 @@ import classNames from "classnames";
 import SubsumIndicator from "@/../public/icon_subsum_indicator.svg";
 import { MdTypography } from "@/app/components/typography";
 import { FileDownloadOutlined } from "@mui/icons-material";
+import { menuItems } from "@/app/util/constants";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
 
 type GuideItemProps = {
   id: string;
@@ -20,168 +22,34 @@ type GuideCategoryProps = {
   items: GuideItemProps[];
 };
 
-const categories: GuideCategoryProps[] = [
-  {
-    title: "Schedule",
-    items: [
-      {
-        id: "ptp",
-        title: "Point to Point Schedule",
-        downloadLink: "",
-      },
-      {
-        id: "vessel",
-        title: "Vessel Schedule",
-        downloadLink: "",
-      },
-      {
-        id: "port",
-        title: "Port Schedule",
-        downloadLink: "",
-      },
-      {
-        id: "long-range",
-        title: "Long Range Schedule",
-        downloadLink: "",
-      },
-      {
-        id: "my",
-        title: "My Schedule",
-        downloadLink: "",
-      },
-    ],
-  },
-  {
-    title: "Outbound",
-    items: [
-      {
-        id: "precheck",
-        title: "Pre Check",
-        downloadLink: "",
-      },
-      {
-        id: "booking",
-        title: "Booking",
-        downloadLink: "",
-      },
-      {
-        id: "si",
-        title: "Shipping Instruction",
-        downloadLink: "",
-      },
-      {
-        id: "sea-waybill",
-        title: "Sea Waybill",
-        downloadLink: "",
-      },
-      {
-        id: "simple-si",
-        title: "Simple S/I",
-        downloadLink: "",
-      },
-      {
-        id: "vgm-submit",
-        title: "VGM Submit Guide",
-        downloadLink: "",
-      },
-    ],
-  },
-  {
-    title: "Inbound",
-    items: [
-      {
-        id: "inbound-master",
-        title: "Inbound Master",
-        downloadLink: "",
-      },
-      {
-        id: "bl-surrender",
-        title: "B/L Surrender Check",
-        downloadLink: "",
-      },
-      {
-        id: "arrival-notice",
-        title: "Arrival Notice",
-        downloadLink: "",
-      },
-      {
-        id: "bl-check",
-        title: "B/L Check",
-        downloadLink: "",
-      },
-    ],
-  },
-  {
-    title: "Track & Trace",
-    items: [
-      {
-        id: "cargo",
-        title: "Cargo Tracking",
-        downloadLink: "",
-      },
-      {
-        id: "my",
-        title: "My Tracking",
-        downloadLink: "",
-      },
-      {
-        id: "visibility",
-        title: "Visibility Service",
-        downloadLink: "",
-      },
-    ],
-  },
-  {
-    title: "Report",
-    items: [
-      {
-        id: "report",
-        title: "Report",
-        downloadLink: "",
-      },
-      {
-        id: "my",
-        title: "My Report",
-        downloadLink: "",
-      },
-    ],
-  },
-  {
-    title: "Rate & Tariff",
-    items: [
-      {
-        id: "dem-det",
-        title: "DEM/DET",
-        downloadLink: "",
-      },
-    ],
-  },
-  {
-    title: "e-Subscription",
-    items: [
-      {
-        id: "e-subscription",
-        title: "e-Subscription",
-        downloadLink: " ",
-      },
-    ],
-  },
-];
-
 export default function GuideDownloadPage() {
   const cx = classNames.bind(styles);
 
   return (
-    <div aria-label="container" className={cx(styles.container, "h-full ")}>
-      <PageTitle title="e-Service Guide" hasFavorite={false} />
-      <div className={cx(styles.area, "flex-1")}>
-        <div className="grid grid-cols-4 gap-8">
-          {categories.map((category) => (
-            <GuideCategory key={category.title} {...category} />
-          ))}
+    <OverlayScrollbarsComponent defer className="h-full mx-2">
+      <div aria-label="container" className={cx(styles.container, "h-fit")}>
+        <PageTitle title="e-Service Guide" hasFavorite={false} />
+        <div className={cx(styles.area, "h-fit flex-1")}>
+          <div className="grid grid-cols-4 gap-8">
+            {menuItems
+              .filter((item) => item.id !== "dashboard")
+              .map((item) => (
+                <GuideCategory
+                  key={item.id}
+                  title={item.name}
+                  items={item.subMenu!.map((subItem) => {
+                    return {
+                      id: subItem.id,
+                      title: subItem.name,
+                      downloadLink: subItem.link,
+                    } as GuideItemProps;
+                  })}
+                />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+    </OverlayScrollbarsComponent>
   );
 }
 
