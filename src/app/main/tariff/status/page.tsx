edef7b1a9928e3e019ed4ec62b56768832d5302a
SulfurBottom <397love@gmail.com>
2024-06-04 10:44:02 +0900

    refactor: Update DetentionStatusPage UI layout and add search functionality

--- src/app/main/tariff/status/page.tsx
+++ src/app/main/tariff/status/page.tsx
@@ -8,22 +8,27 @@ import PageTitle from "@/app/components/title-components";
 import { MdTypography } from "@/app/components/typography";
 import styles from "@/app/styles/base.module.css";
 import {
+  MdChipSet,
   MdFilledButton,
+  MdOutlinedButton,
   MdOutlinedSegmentedButton,
   MdOutlinedSegmentedButtonSet,
+  MdOutlinedTextField,
   MdRadio,
   MdTextButton,
 } from "@/app/util/md3";
 import { faker } from "@faker-js/faker";
 import { useMemo, useState } from "react";
 import { DetentionStatusTable } from "./table";
+import { RemovableChip } from "@/app/components/removable-chip";
 
 export default function DetentionStatusPage() {
   const [pageState, setPageState] = useState<"unsearch" | "search">("unsearch");
-  const [currentTab, setCurrentTab] = useState<"outbound" | "inbound">(
-    "outbound"
+  const [boundSelection, setBoundSelection] = useState<"outbound" | "inbound">(
+    `outbound`
   );
   const [searchType, setSearchType] = useState<"gate" | "number">("gate");
+  const [numberQueries, setNumberQueries] = useState<string[]>([]);
   const tempContracts = useMemo(() => {
     return Array.from({ length: 10 }, (_, index) =>
       faker.string.alphanumeric(10).toUpperCase()
@@ -36,17 +41,100 @@ export default function DetentionStatusPage() {
       <div className={styles.area}>
         <MdOutlinedSegmentedButtonSet>
           <MdOutlinedSegmentedButton
-            label="Outbound (Export)"
-            selected={currentTab === "outbound"}
-            onClick={() => setCurrentTab("outbound")}
+            label="Gate Out Date"
+            selected={searchType === "gate"}
+            onClick={() => setSearchType("gate")}
           />
           <MdOutlinedSegmentedButton
-            label="Inbound (Import)"
-            selected={currentTab === "inbound"}
-            onClick={() => setCurrentTab("inbound")}
+            label="Booking No. or B/L No."
+            selected={searchType === "number"}
+            onClick={() => setSearchType("number")}
           />
         </MdOutlinedSegmentedButtonSet>
-        <div className="flex gap-4">
+        <div className="flex gap-4 px-1 py-1">
+          <MdTypography variant="label" size="large" tag="label">
+            <MdRadio
+              className="mr-2"
+              name="bound"
+              checked={boundSelection === "outbound"}
+              onClick={() => setBoundSelection("outbound")}
+            />
+            Outbound
+          </MdTypography>
+          <MdTypography variant="label" size="large" tag="label">
+            <MdRadio
+              className="mr-2"
+              name="bound"
+              checked={boundSelection === "inbound"}
+              onClick={() => setBoundSelection("inbound")}
+            />
+            Inbound
+          </MdTypography>
+        </div>
+
+        <div className="flex gap-4 items-end">
+          <div className="flex-1">
+            {searchType === "gate" && (
+              <div className="flex gap-4 items-center">
+                <DateRangePicker label="Date" />
+                <NAOutlinedListBox
+                  label="By Contract"
+                  options={tempContracts}
+                />
+                <MdOutlinedButton>Update Contract No.</MdOutlinedButton>
+              </div>
+            )}
+            {searchType === "number" && (
+              <div className="flex flex-col gap-2">
+                <MdOutlinedTextField
+                  label="Booking No. or B/L No."
+                  value=""
+                  className="w-1/2"
+                  onKeyDown={(e) => {
+                    const query = e.currentTarget.value;
+
+                    if (e.key === "Enter") {
+                      if (query === "" || numberQueries.includes(query)) return;
+                      setNumberQueries([...numberQueries, query]);
+                      e.currentTarget.value = "";
+                    }
+                  }}
+                />
+                <MdChipSet>
+                  {numberQueries.map((query, index) => (
+                    <RemovableChip
+                      key={query}
+                      label={query}
+                      onRemove={() => {
+                        setNumberQueries(
+                          numberQueries.filter((_, i) => i !== index)
+                        );
+                      }}
+                    />
+                  ))}
+                </MdChipSet>
+              </div>
+            )}
+          </div>
+          <div className="flex gap-4 justify-end">
+            <MdTextButton
+              onClick={() => {
+                setPageState("unsearch");
+              }}
+            >
+              Reset
+            </MdTextButton>
+            <MdFilledButton
+              onClick={() => {
+                setPageState("search");
+              }}
+            >
+              Search
+            </MdFilledButton>
+          </div>
+        </div>
+
+        {/* <div className="flex gap-4">
           <MdTypography
             variant="label"
             size="large"
@@ -99,7 +187,7 @@ export default function DetentionStatusPage() {
           >
             Search
           </MdFilledButton>
-        </div>
+        </div> */}
       </div>
       <div className={styles.area}>
         {pageState === "search" ? (
