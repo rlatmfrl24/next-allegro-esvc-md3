1ae0056b17414b563ac7e78b1ac7fc1a51ca3bfc
SulfurBottom <397love@gmail.com>
2024-06-05 16:07:06 +0900

    refactor: Update contract number selector component

--- src/app/main/shipment/report/page.tsx
+++ src/app/main/shipment/report/page.tsx
@@ -43,6 +43,7 @@ import { basicPopoverStyles } from "@/app/util/constants";
 import { DividerComponent } from "@/app/components/divider";
 import { flushSync } from "react-dom";
 import { ReportTable } from "./table";
+import { ContractNumberSelector } from "@/app/components/update-contract-number";
 
 const MoreFilter = (props: { onFilterChange: (filter: string[]) => void }) => {
   const [moreFilter, setMoreFilter] = useState<string[]>([]);
@@ -340,12 +341,7 @@ export default function ShipmentReportPage() {
                   />
                 ) : (
                   <>
-                    <NAOutlinedListBox
-                      options={tempContracts}
-                      label="By Contract"
-                      className="min-w-24"
-                    />
-                    <MdOutlinedButton>Update Contract No.</MdOutlinedButton>
+                    <ContractNumberSelector contracts={tempContracts} />
                   </>
                 )}
                 <MyReportComponent />
