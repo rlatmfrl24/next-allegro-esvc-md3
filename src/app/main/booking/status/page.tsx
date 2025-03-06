d9a93466d8d66766b7eb09d14306b2319e8df00b
Sulfurbottom <397love@gmail.com>
2025-03-06 14:25:51 +0900

    코드 포맷 정리: JSX 코드의 들여쓰기 및 공백 일관성 개선

--- src/app/main/booking/status/page.tsx
+++ src/app/main/booking/status/page.tsx
@@ -10,32 +10,32 @@ import { resetBookingState } from "@/app/store/booking.store";
 import classNames from "classnames";
 
 export default function BookingStatusPage() {
-  const router = useRouter();
-  const reset = useSetRecoilState(resetBookingState);
-  const cx = classNames.bind(styles);
+	const router = useRouter();
+	const reset = useSetRecoilState(resetBookingState);
+	const cx = classNames.bind(styles);
 
-  return (
-    <div aria-label="container" className={cx(styles.container)}>
-      <div className="flex items-center justify-between">
-        <PageTitle
-          title="Booking Status"
-          category="Booking"
-          href="/main/booking/status"
-        />
-        <MdOutlinedButton
-          onClick={() => {
-            reset();
-            router.push("/main/booking/request");
-          }}
-        >
-          New Booking
-        </MdOutlinedButton>
-      </div>
-      <BookingStatusCondition />
+	return (
+		<div aria-label="container" className={cx(styles.container)}>
+			<div className="flex items-center justify-between">
+				<PageTitle
+					title="Booking Status"
+					category="Booking"
+					href="/main/booking/status"
+				/>
+				<MdOutlinedButton
+					onClick={() => {
+						reset();
+						router.push("/main/booking/request");
+					}}
+				>
+					New Booking
+				</MdOutlinedButton>
+			</div>
+			<BookingStatusCondition />
 
-      <div className={cx(styles.area, styles.table)}>
-        <BookingStatusTable />
-      </div>
-    </div>
-  );
+			<div className={cx(styles.area, styles.table)}>
+				<BookingStatusTable />
+			</div>
+		</div>
+	);
 }
--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -6,42 +6,43 @@ import { useSetRecoilState } from "recoil";
 import { ScrollState } from "../store/global.store";
 
 export default function MainLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  const scrollRef = useRef<any>();
-  const setScrollState = useSetRecoilState(ScrollState);
-  const [initialize, instance] = useOverlayScrollbars({
-    defer: true,
-    events: {
-      scroll: (instance) => {
-        const viewport = instance.elements().viewport;
-        setScrollState((prevState) => ({
-          ...prevState,
-          xPosition: viewport.scrollLeft,
-          yPosition: viewport.scrollTop,
-        }));
-      },
-    },
-  });
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const scrollRef = useRef<any>();
+	const setScrollState = useSetRecoilState(ScrollState);
+	const [initialize, instance] = useOverlayScrollbars({
+		defer: true,
+		events: {
+			scroll: (instance) => {
+				const viewport = instance.elements().viewport;
+				setScrollState((prevState) => ({
+					...prevState,
+					xPosition: viewport.scrollLeft,
+					yPosition: viewport.scrollTop,
+				}));
+			},
+		},
+	});
 
-  useEffect(() => {
-    if (scrollRef.current) {
-      initialize(scrollRef.current);
-    }
+	useEffect(() => {
+		if (scrollRef.current) {
+			initialize(scrollRef.current);
+		}
 
-    setScrollState((prevState) => ({
-      ...prevState,
-      instance: instance,
-    }));
-  }, [initialize, instance, setScrollState]);
+		setScrollState((prevState) => ({
+			...prevState,
+			instance: instance,
+		}));
+	}, [initialize, instance, setScrollState]);
 
-  return (
-    <div className="relative flex h-full mx-1 py-2">
-      <div className="w-full flex" ref={scrollRef}>
-        <div className="h-full flex flex-col">{children}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="relative flex h-full mx-1 py-2">
+			<div className="w-full flex" ref={scrollRef}>
+				<div className="h-full flex flex-col">{children}</div>
+			</div>
+		</div>
+	);
 }
