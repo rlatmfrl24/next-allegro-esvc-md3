0081f52cc68f29ea9791e8c03cefe1f4de066b74
Sulfurbottom <397love@gmail.com>
2025-03-27 08:52:11 +0900

    C/N Issue 알림 다이얼로그 추가 및 상태 관리 개선

--- src/app/main/documents/si/table-action-buttons.tsx
+++ src/app/main/documents/si/table-action-buttons.tsx
@@ -29,6 +29,7 @@ import { createColumnHelper } from "@tanstack/react-table";
 
 import { SimpleItem } from "../../booking/request/components/base";
 import { SIStateChip } from "./si-state-chip";
+import NaToggleButton from "@/app/components/na-toggle-button";
 
 const ActionButtons = ({
 	selectionList,
@@ -43,6 +44,7 @@ const ActionButtons = ({
 	const [isBLPreviewOpen, setIsBLPreviewOpen] = useState(false);
 	const [isBLCombineOpen, setIsBLCombineOpen] = useState(false);
 	const [isBLIssueRequestOpen, setIsBLIssueRequestOpen] = useState(false);
+	const [isCNIssueAlertOpen, setIsCNIssueAlertOpen] = useState(false);
 
 	return (
 		<>
@@ -112,12 +114,22 @@ const ActionButtons = ({
 						</MdOutlinedButton>
 					)}
 					{states.includes(SIState.BLIssueClosed) && (
-						<MdOutlinedButton onClick={() => {}}>C/N Issue</MdOutlinedButton>
+						<MdOutlinedButton
+							onClick={() => {
+								setIsCNIssueAlertOpen(true);
+							}}
+						>
+							C/N Issue
+						</MdOutlinedButton>
 					)}
 				</div>
 			)}
 			<Portal selector="#main-container">
 				<BLPreview open={isBLPreviewOpen} onOpenChange={setIsBLPreviewOpen} />
+				<CNIssueAlert
+					open={isCNIssueAlertOpen}
+					onOpenChange={setIsCNIssueAlertOpen}
+				/>
 				<BLIssueRequest
 					open={isBLIssueRequestOpen}
 					onOpenChange={setIsBLIssueRequestOpen}
@@ -411,6 +423,65 @@ const BLCombine = ({
 					}}
 				>
 					Combine
+				</MdFilledButton>
+			</div>
+		</MdDialog>
+	);
+};
+
+const CNIssueAlert = ({
+	open,
+	onOpenChange,
+}: {
+	open: boolean;
+	onOpenChange: (open: boolean) => void;
+}) => {
+	const [isChecked, setIsChecked] = useState(false);
+	const router = useRouter();
+
+	return (
+		<MdDialog
+			open={open}
+			closed={() => onOpenChange(false)}
+			className="max-w-[520px]"
+		>
+			<div slot="headline">Alert Message</div>
+			<div slot="content">
+				<p>
+					The selected B/L has exceeded the documentation closing deadline. Any
+					modifications you input will be processed as a Correction Notice.
+					Please be aware that any issues or damages (including financial
+					losses) arising from these modifications will be your responsibility.
+				</p>
+				<label
+					htmlFor="alert-checkbox"
+					className="flex items-center gap-3 mt-4"
+				>
+					<MdCheckbox
+						id="alert-checkbox"
+						className="w-7"
+						onInput={(e) => {
+							console.log(e.currentTarget.checked);
+							setIsChecked(e.currentTarget.checked);
+						}}
+					/>
+					<p>
+						We will take responsibility for any issues related to the
+						modifications entered, based on the above notice.
+					</p>
+				</label>
+			</div>
+			<div slot="actions">
+				<MdOutlinedButton onClick={() => onOpenChange(false)}>
+					Cancel
+				</MdOutlinedButton>
+				<MdFilledButton
+					disabled={!isChecked}
+					onClick={() => {
+						router.push("/main/documents/si/edit");
+					}}
+				>
+					Request
 				</MdFilledButton>
 			</div>
 		</MdDialog>
